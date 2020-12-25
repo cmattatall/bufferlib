ea8cd3c164bdfd56ca0ab6c12912354867cabe8f
Carl Mattatall <cmattatall2@gmail.com>
2020-12-25 13:00:08 -0400

    add test for too many reads

--- test/too_many_reads.c
+++ test/too_many_reads.c
@@ -15,10 +15,49 @@
 
 #include "bufferlib.h"
 
+#define TEST_BUF_SIZE 50
 
-int main(void)
+
+int main(int argc, char **argv)
 {
+    int          status = 0;
+    unsigned int size;
+    for (size = 1; (status == 0) && (size < 2000); size++)
+    {
+        buffer_handle ringbuf = bufferlib_ringbuf(size);
+        int           i;
+        int           byte = '!';
+        for (i = 0; (status == 0) && (i < size); i++)
+        {
+            int byte_written = ringbuf.write_next(ringbuf.this, (char)byte);
+            if (byte_written != byte)
+            {
+                status = 1;
+            }
+        }
 
+        for (i = 0; (status == 0) && (i < size); i++)
+        {
+            int byte_read = ringbuf.read_next(ringbuf.this);
+            if (byte_read != byte)
+            {
+                status = 1;
+            }
+        }
 
-    return 0;
+        /* After we've read out as many bytes as we have written,
+         * next read should fail
+         */
+        if (status == 0)
+        {
+            int byte_read = ringbuf.read_next(ringbuf.this);
+            if (byte_read != BUFFERLIB_READ_FAILURE)
+            {
+                status = 1;
+            }
+        }
+
+        ringbuf.delete(ringbuf.this);
+    }
+    return status;
 }
